6ae81df9b0182f103d6798305a46e002c49af1f2
Pablo Cordero Romero <pablocorderoromero@gmail.com>
2021-09-17 09:15:51 +0200

    refactor: Initial store set empty

--- src/store.js
+++ src/store.js
@@ -3,10 +3,7 @@ import booksReducer from "./reducers/books";
 import thunk from 'redux-thunk';
 
 const INITIAL_STATE = {
-    books: { 
-        "1": {id: "1", title: "Harry Potter", description: "A young magician", author: "J.K. Rowling"},
-        "2": {id: "2", title: "Programming with Python", description: "Learn programming", author: "Pablo Cordero"}
-    }
+    books: {}
 }
 
 const middlewares = [thunk];
