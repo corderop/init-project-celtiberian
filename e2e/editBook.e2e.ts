9582cc5af349f0c1f4687150d29546fb7cc69087
Pablo Cordero Romero <pablocorderoromero@gmail.com>
2021-09-28 17:51:13 +0200

    test: e2e test with the action of edit books

---
+++ e2e/editBook.e2e.ts
@@ -0,0 +1,33 @@
+import { by, device, expect, element } from 'detox'
+
+describe('Adding books works properly', () => {
+  beforeAll(async () => {
+    await device.launchApp()
+  })
+
+  test('It has a list of books', async () => {
+    // Check the navbar has two elements
+    await expect(element(by.id('booksComponent.bookList'))).toBeVisible()
+    await expect(element(by.id('booksComponent.bookList.1'))).toBeVisible()
+    await expect(element(by.id('booksComponent.bookList.2'))).toBeVisible()
+  })
+
+  test('Navigate to the book info', async () => {
+    await element(by.id('booksComponent.bookList.1')).tap()
+    await expect(element(by.id('bookComponent'))).toBeVisible()
+  })
+
+  test('Form fields and buttons visible', async () => {
+    // Check the form is correct
+    await expect(element(by.id('bookComponent.bookInfo.title'))).toBeVisible()
+    await expect(element(by.id('bookComponent.bookInfo.title'))).toBeVisible()
+    await expect(element(by.id('bookComponent.bookInfo.title'))).toBeVisible()
+
+    await expect(
+      element(by.id('bookComponent.bookInfo.buttonWrap.0'))
+    ).toBeVisible()
+    await expect(
+      element(by.id('bookComponent.bookInfo.buttonWrap.1'))
+    ).toBeVisible()
+  })
+})
